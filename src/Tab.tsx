c71e388a11ecb2c02db6314cf8d354afe0135c0d
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-06-28 08:46:54 +0000

    change

--- src/Tab.tsx
+++ src/Tab.tsx
@@ -122,13 +122,13 @@ export default function Tabs() {
               <a href="" target="_blank">
                 <h3>Winter Project 2024 (Co-Exist)</h3>
               </a>
-              <h4>06/2024-07/2024</h4>
+              <h4></h4>
             </div>
             <div id="edbot">
               <img src="location.png" />
               <h4>Perth</h4>
               <img src="suitcase.png" />
-              <h4>Volunteer Developer</h4>
+              <h4>Developer</h4>
             </div>
             <ul>
               
